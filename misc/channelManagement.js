d19995ae1fe36fba3da382e2cf03d581e28c90ce
Oliver Henrichs <Oliver Henrichs>
2021-02-15 19:44:54 +0100

    Signed-off-by: Oliver Henrichs <Oliver Henrichs>

--- misc/channelManagement.js
+++ misc/channelManagement.js
@@ -1,6 +1,13 @@
-const lc = [process.env.BOT_LOBBY_CHANNEL_EU, process.env.BOT_LOBBY_CHANNEL_NA, process.env.BOT_LOBBY_CHANNEL_SEA, process.env.BOT_LOBBY_CHANNEL_TRYOUT, process.env.BOT_LOBBY_CHANNEL_TEST, process.env.BOT_LOBBY_CHANNEL_BOTBASH]
+const lc = [process.env.BOT_LOBBY_CHANNEL_EU, process.env.BOT_LOBBY_CHANNEL_NA, process.env.BOT_LOBBY_CHANNEL_SEA, process.env.BOT_LOBBY_CHANNEL_TRYOUT, process.env.BOT_LOBBY_CHANNEL_TEST, process.env.BOT_LOBBY_CHANNEL_BOTBASH, process.env.BOT_LOBBY_CHANNEL_REPLAYANALYSIS]
 const sc = [process.env.BOT_SCHEDULE_CHANNEL_BOTBASH, process.env.BOT_SCHEDULE_CHANNEL_TRYOUT, process.env.BOT_SCHEDULE_CHANNEL_5V5];
-const cs = "<#" + process.env.BOT_LOBBY_CHANNEL_1 + ">, <#" + process.env.BOT_LOBBY_CHANNEL_2 + ">, <#" + process.env.BOT_LOBBY_CHANNEL_3 + ">, <#" + process.env.BOT_LOBBY_CHANNEL_4+ ">, <#" + process.env.BOT_LOBBY_CHANNEL_5 + ">, <#" + process.env.BOT_LOBBY_CHANNEL_BOTBASH + ">"
+const cs = "<#" 
+            + process.env.BOT_LOBBY_CHANNEL_EU + ">, <#" 
+            + process.env.BOT_LOBBY_CHANNEL_NA + ">, <#" 
+            + process.env.BOT_LOBBY_CHANNEL_SEA + ">, <#" 
+            + process.env.BOT_LOBBY_CHANNEL_TRYOUT + ">, <#" 
+            + process.env.BOT_LOBBY_CHANNEL_TEST + ">, <#" 
+            + process.env.BOT_LOBBY_CHANNEL_REPLAYANALYSIS + ">, <#"
+            + process.env.BOT_LOBBY_CHANNEL_BOTBASH + ">";
 
 // channel management
 module.exports = {
